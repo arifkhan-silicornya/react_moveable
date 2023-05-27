57e35b88194195d96182361d36a7ff5a524001f4
arifkhan-silicornya <arifkhanshanto123@gmail.com>
2023-05-27 02:10:50 -0700

    project 1st initialized

---
+++ react-moveable/src/App.js
@@ -0,0 +1,28 @@
+
+import * as React from "react";
+import Moveable from "react-moveable";
+import './App.css';
+
+
+function App() {
+  const targetRef = React.useRef("null");
+  return (
+    <div className="App">
+      <div className="container">
+                <div className="target" ref={targetRef}>Target</div>
+                <Moveable
+                    target={targetRef}
+                    draggable={true}
+                    throttleDrag={1}
+                    edgeDraggable={false}
+                    startDragRotate={0}
+                    throttleDragRotate={0}
+                    onDrag={e => {e.target.style.transform = e.transform;
+                    }}
+                />
+            </div>
+    </div>
+  );
+}
+
+export default App;
